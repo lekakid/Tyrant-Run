2179832d203c61aa20dbd8bb9f110ff1d1facc47
LeKAKiD <lekakid@kakao.com>
2020-04-08 17:14:34 +0900

    리소스 준비 됨, 공룡 점프할 줄 앎

--- Assets/Scripts/Dino.cs
+++ Assets/Scripts/Dino.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Dino : MonoBehaviour
 {
+    Rigidbody2D rb;
+    bool isJumped;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space) && !isJumped) {
+            isJumped = true;
+            rb.AddForce(Vector2.up * 45f, ForceMode2D.Impulse);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.collider.CompareTag("Floor")) {
+            isJumped = false;
+        }
     }
 }
