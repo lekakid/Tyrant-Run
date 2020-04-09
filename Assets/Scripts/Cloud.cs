db4876e5e918a96fbca3c28aa87dd1a10fbdb8aa
LeKAKiD <lekakid@kakao.com>
2020-04-09 16:40:30 +0900

    아이콘 설정, 구름 추가

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (Vector3)(Vector2.left * 2f * Time.deltaTime);
+
+        if(transform.position.x < -21f) {
+            transform.position = new Vector2(Random.Range(21f, 24f), Random.Range(0f, 3f));
+        }
+    }
+}
